235b2aeeefd56c0b416339ae985c6be82dbf14e1
John Palacios <johnpalacios.t@gmail.com>
2023-11-17 16:48:29 -0500

    Add Faker gem and created SEEDS for the tables.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# seeds.rb
+require 'faker'
+
+# Create Users
+10.times do
+  User.create(name: Faker::Name.name)
+end
+
+# Create Movements
+10.times do
+  Movement.create(author_id: User.pluck(:id).sample, name: Faker::Commerce.product_name, amount: Faker::Number.decimal(l_digits: 2))
+end
+
+# Create Groups
+20.times do
+  Group.create(user_id: User.pluck(:id).sample, movement_id: Movement.pluck(:id).sample, name: Faker::Team.name, icon: Faker::Lorem.word)
+end
