d3b21eb7e5a7d86ac2629143caa9c3b13c7f039b
John Palacios <johnpalacios.t@gmail.com>
2023-11-20 16:42:23 -0500

    Skip users table.

--- db/migrate/20231117211805_create_users.rb
+++ db/migrate/20231117211805_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
-    create_table :users do |t|
-      t.string :name
-    end
+    # Users on Render
+    # create_table :users do |t|
+    #   t.string :name
+    # end
   end
 end
